fdaa2d1a0051c51affbb16d4dcba5a9d0d255030
Angga Pramudya <anggapramudya58@yahoo.com>
2017-08-08 16:32:48 +0700

    Initial Commit

---
+++ app/Ticket.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $guarded = ['id'];
+    public function comments()
+    {
+        return $this->hasMany('App\Comment', 'post_id');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'TicketsController@create');
+Route::post('/contact', 'TicketsController@store');
+Route::get('/tickets', 'TicketsController@index');
+Route::get('/ticket/{slug?}', 'TicketsController@show');
+Route::get('/ticket/{slug?}/edit', 'TicketsController@edit');
+Route::post('/ticket/{slug?}/edit','TicketsController@update');
+Route::post('/ticket/{slug?}/delete', 'TicketsController@destroy');
+Route::post('/comment', 'CommentsController@newComment');
+
+Route::get('sendemail', function () {
+
+    $data = array(
+        'name' => "Learning Laravel",
+    );
+
+    Mail::send('emails.welcome', $data, function ($message) {
+
+        $message->from('suichi007@gmail.com', 'Learning Laravel');
+
+        $message->to('suichi007@gmail.com')->subject('Learning Laravel test email');
+
+    });
+
+    return "Your email has been sent successfully";
+
+});
